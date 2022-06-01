b4ca8cbfd67796736d811f8eac71c3e7de56b2ce
L1Y2G31227 <17695619597>
2022-06-01 09:42:05 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"douyin/repository"
+	"douyin/server"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.Use(gin.Logger())
+
+	server.InitRouter(r)
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	repository.InitMysql()
+
+}
---
+++ repository/follow.go
@@ -0,0 +1,7 @@
+package repository
+
+type Follow struct {
+	Id         int64  `gorm:"column:id"`    // 关注记录ID
+	FollowerId string `gorm:"column:title"` // 关注者用户ID
+	StarId     string `gorm:"column:name"`  // 被关注者用户ID
+}
