a9880b270a64e8f1d95b24ef2cd2d8aeab098ec5
xuanxuanpi <1106232975@qq.com>
2022-06-06 21:02:36 +0800

    修改返回信息位置

--- model/video.go
+++ model/video.go
@@ -17,6 +17,18 @@ func (Video) TableName() string {
 	return "videos"
 }
 
+// VideoInfo publishlist 需要返回给用户的Video信息
+type VideoInfo struct {
+	ID            int64    `json:"id"`
+	Author        UserInfo `json:"author"`
+	PlayUrl       string   `json:"play_url"`
+	CoverUrl      string   `json:"cover_url"`
+	FavoriteCount int64    `json:"favorite_count"`
+	CommentCount  int64    `json:"comment_count"`
+	IsFavorite    bool     `json:"is_favorite"`
+	Title         string   `json:"title"`
+}
+
 // GetPublishList 获取发布列表
 func GetPublishList(userId int64) []Video {
 	var videos []Video
