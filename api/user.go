2cc07cdadc8ba8e11223b38123599db99eee71e2
L1Y2G31227 <17695619597>
2022-06-06 19:09:31 +0800

    Merge remote-tracking branch 'origin/user_version2' into user_version2
    # Conflicts:
    #	api/user.go
    
    修改了userinfo的statuscode

--- api/user.go
+++ api/user.go
@@ -56,7 +56,7 @@ func UserLogin(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, UserLoginResponse{
-		Response: Response{StatusCode: int32(code), StatusMsg: errmsg.GetErrMsg(code)},
+		Response: Response{StatusCode: 0, StatusMsg: errmsg.GetErrMsg(code)},
 		UserId:   data.ID,
 		Token:    token,
 	})
@@ -135,76 +135,3 @@ var usersLoginInfo = map[string]User{
 		IsFollow:      true,
 	},
 }
-
-//
-//var userIdSequence = int64(1)
-//
-//type UserLoginResponse struct {
-//	Response
-//	UserId int64  `json:"user_id,omitempty"`
-//	Token  string `json:"token"`
-//}
-//
-//type UserResponse struct {
-//	Response
-//	User User `json:"user"`
-//}
-//
-//func Register(c *gin.Context) {
-//	username := c.Query("username")
-//	password := c.Query("password")
-//
-//	token := username + password
-//
-//	if _, exist := usersLoginInfo[token]; exist {
-//		c.JSON(http.StatusOK, UserLoginResponse{
-//			Response: Response{StatusCode: 1, StatusMsg: "User already exist"},
-//		})
-//	} else {
-//		atomic.AddInt64(&userIdSequence, 1)
-//		newUser := User{
-//			Id:   userIdSequence,
-//			Name: username,
-//		}
-//		usersLoginInfo[token] = newUser
-//		c.JSON(http.StatusOK, UserLoginResponse{
-//			Response: Response{StatusCode: 0},
-//			UserId:   userIdSequence,
-//			Token:    username + password,
-//		})
-//	}
-//}
-//
-//func Login(c *gin.Context) {
-//	username := c.Query("username")
-//	password := c.Query("password")
-//
-//	token := username + password
-//
-//	if user, exist := usersLoginInfo[token]; exist {
-//		c.JSON(http.StatusOK, UserLoginResponse{
-//			Response: Response{StatusCode: 0},
-//			UserId:   user.Id,
-//			Token:    token,
-//		})
-//	} else {
-//		c.JSON(http.StatusOK, UserLoginResponse{
-//			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-//		})
-//	}
-//}
-//
-//func UserInfo(c *gin.Context) {
-//	token := c.Query("token")
-//
-//	if user, exist := usersLoginInfo[token]; exist {
-//		c.JSON(http.StatusOK, UserResponse{
-//			Response: Response{StatusCode: 0},
-//			User:     user,
-//		})
-//	} else {
-//		c.JSON(http.StatusOK, UserResponse{
-//			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-//		})
-//	}
-//}
